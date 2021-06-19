8d29b2c762b7ddee5245962ee5e6db001b4d90b0
nishkakotian <nishka308@gmail.com>
2021-06-19 20:07:59 +0530

    Fixed Start Webcam button not working

--- script.js
+++ script.js
@@ -32,50 +32,6 @@ async function init() {
     }
 }
 
-$(document).ready(function () {
-    $("#loadBtn").on("click", function () {
-        const fileUploadControl = $("#fruitimg")[0];
-        if (fileUploadControl.files.length > 0) {
-
-            const modelURL = URL + "model.json";
-            const metadataURL = URL + "metadata.json";
-
-            // load the model and metadata
-            model = await tmImage.load(modelURL, metadataURL);
-            maxPredictions = model.getTotalClasses();
-
-            const file = fileUploadControl.files[0];
-
-            const name = "photo.jpg";
-            const parseFile = new Parse.File(name, file);
-
-            parseFile.save().then(function () {
-                // The file has been saved to Parse
-                document.getElementById("uploadedImage").src = parseFile.url();
-
-                const prediction = await model.predict(parseFile.url());
-                var bestPrediction = "";
-
-                for (let i = 0; i < maxPredictions; i++) {
-                    const classPrediction =
-                        prediction[i].className + ": " + prediction[i].probability.toFixed(2);
-                    labelContainer.childNodes[i].innerHTML = classPrediction;
-                }
-
-            }, function (error) {
-                // The file either could not be read, or could not be saved to Parse.
-                document.getElementById("result").innerHTML = "NOo Success!";
-            });
-
-
-
-        }
-        else {
-            document.getElementById("result").innerHTML = "did not work only!";
-        }
-    });
-});
-
 async function loop() {
     webcam.update(); // update the webcam frame
     await predict();
@@ -92,3 +48,4 @@ async function predict() {
         labelContainer.childNodes[i].innerHTML = classPrediction;
     }
 }
+

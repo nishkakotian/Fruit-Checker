2b163e7d9e479b6e517f4ae9aa48668da574ff1a
nishkakotian <nishka308@gmail.com>
2021-06-20 10:52:56 +0530

    Webcam based detection fully complete

--- script.js
+++ script.js
@@ -4,12 +4,32 @@ Parse.initialize(
 );
 Parse.serverURL = 'https://pg-app-s4xeiqcvd9vyl5lycfvuzz49fbtelr.scalabl.cloud/1/';
 
-const URL = "https://teachablemachine.withgoogle.com/models/r6WxaGerd/";
+const URL = "https://teachablemachine.withgoogle.com/models/N58PlX_GN/";
 
-let model, webcam, labelContainer, maxPredictions;
+let model, webcam, newlabel, labelContainer, maxPredictions, camera_on = false;
+
+function useWebcam() {
+    camera_on = !camera_on;
+
+    if (camera_on) {
+        init();
+        document.getElementById("webcam").innerHTML = "Close Webcam";
+    }
+    else {
+        stopWebcam();
+        document.getElementById("webcam").innerHTML = "Start Webcam";
+    }
+}
+
+async function stopWebcam() {
+    await webcam.stop();
+    document.getElementById("webcam-container").removeChild(webcam.canvas);
+    labelContainer.removeChild(newlabel);
+}
 
 // Load the image model and setup the webcam
 async function init() {
+
     const modelURL = URL + "model.json";
     const metadataURL = URL + "metadata.json";
 
@@ -24,12 +44,13 @@ async function init() {
     await webcam.play();
     window.requestAnimationFrame(loop);
 
-    // append elements to the DOM
+    // append element to the DOM
     document.getElementById("webcam-container").appendChild(webcam.canvas);
+
+    newlabel = document.createElement("div");
+
     labelContainer = document.getElementById("label-container");
-    for (let i = 0; i < maxPredictions; i++) { // and class labels
-        labelContainer.appendChild(document.createElement("div"));
-    }
+    labelContainer.appendChild(newlabel);
 }
 
 async function loop() {
@@ -42,10 +63,90 @@ async function loop() {
 async function predict() {
     // predict can take in an image, video or canvas html element
     const prediction = await model.predict(webcam.canvas);
+
+    var highestVal = 0.00;
+    var bestClass = "";
+    result = document.getElementById("label-container");
     for (let i = 0; i < maxPredictions; i++) {
-        const classPrediction =
-            prediction[i].className + ": " + prediction[i].probability.toFixed(2);
-        labelContainer.childNodes[i].innerHTML = classPrediction;
+        var classPrediction = prediction[i].probability.toFixed(2);
+        if (classPrediction > highestVal) {
+            highestVal = classPrediction;
+            bestClass = prediction[i].className;
+        }
     }
+
+    if (bestClass == "Fresh Banana" || bestClass == "Fresh Apple" || bestClass == "Fresh Orange") {
+        newlabel.className = "alert alert-success";
+    }
+    else {
+        newlabel.className = "alert alert-danger";
+    }
+
+    newlabel.innerHTML = bestClass;
 }
 
+$(document).ready(function () {
+    $("#loadBtn").on("click", async function () {
+
+        const fileUploadControl = $("#fruitimg")[0];
+        if (fileUploadControl.files.length > 0) {
+
+            const modelURL = URL + "model.json";
+            const metadataURL = URL + "metadata.json";
+
+            // load the model and metadata
+            model = await tmImage.load(modelURL, metadataURL);
+            maxPredictions = model.getTotalClasses();
+
+            const file = fileUploadControl.files[0];
+
+            const name = "photo.jpg";
+            const parseFile = new Parse.File(name, file);
+
+            result = document.getElementById("label-container");
+
+            parseFile.save().then(async function () {
+                // The file has been saved to the Parse server
+                imagedisplay = document.getElementById("uploadedImage");
+                imagedisplay.style.display = "";
+
+                imagedisplay.src = parseFile.url();
+                image = document.getElementById("uploadedImage");
+                const pred = await model.predict(image);
+
+                var highestVal = 0.00;
+                var bestClass = "";
+                result = document.getElementById("label-container");
+
+                for (let i = 0; i < maxPredictions; i++) { // and class labels
+                    result.appendChild(document.createElement('div'));
+                }
+
+                for (let i = 0; i < maxPredictions; i++) {
+                    var classPrediction = pred[i].probability.toFixed(2);
+                    if (classPrediction > highestVal) {
+                        highestVal = classPrediction;
+                        bestClass = pred[i].className;
+                    }
+                    result.childNodes[i].innerHTML = classPrediction;
+                }
+
+                if (bestClass == "Fresh Banana" || bestClass == "Fresh Apple" || bestClass == "Fresh Orange") {
+                    result.className = "alert alert-success";
+                }
+                else {
+                    result.className = "alert alert-danger";
+                }
+
+                result.innerHTML = bestClass;
+
+            }, function (error) {
+                // The file either could not be read, or could not be saved to Parse.
+                result.innerHTML = "Uploading your image failed!";
+            });
+        }
+        else {
+            result.innerHTML = "Try Again!";
+        }
+    });
+});
